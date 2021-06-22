15668086138f42ba4fbc54f70bdbb5ebaf84e9c7
Wojtek <wojciech.sek.ws@gmail.com>
2021-06-22 21:02:41 +0200

    startapp

---
+++ src/main/java/com/conferenceIT/conferenceIT/ConferenceItApplication.java
@@ -0,0 +1,13 @@
+package com.conferenceIT.conferenceIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conferenceIT/conferenceIT/ConferenceItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conferenceIT.conferenceIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
